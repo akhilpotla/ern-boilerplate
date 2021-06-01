fdafb8da27a44278428e6df8d3e15b5a87136727
Akhil Potla <akhil4400@gmail.com>
2021-06-01 17:58:37 -0500

    User API Routes & JWT Authentication

--- server.js
+++ server.js
@@ -1,8 +1,24 @@
 const express = require('express');
+const connectDB = require('./config/db');
 
 const app = express();
 
+// Connect Database
+connectDB();
+
+// Init Middleware
+app.use(express.json({
+    extended: false
+}));
+
 app.get('/', (req, res) => res.send('API Running'));
+
+// Define Routes
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/posts', require('./routes/api/posts'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/users', require('./routes/api/users'));
+
 
 const PORT = process.env.PORT || 5000;
 
