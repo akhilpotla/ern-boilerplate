589401cc5be60295acdfa8be8aacdd20db834447
Akhil Potla <akhil4400@gmail.com>
2021-06-13 17:47:13 -0500

    Add experience

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -137,4 +137,68 @@ router.get('/user/:user_id', async (req, res) => {
     }
 });
 
+// @route DELETE api/profile
+// @desc Delete profile, user, and posts
+// @access Private
+router.delete('/', tokenAuth, async (req, res) => {
+    try {
+        // TODO - Remove user's posts
+
+        // Remove profile
+        await Profile.findOneAndRemove({ user: req.user.id });
+        // Remove user
+        await User.findOneAndRemove({ _id: req.user.id });
+
+        res.json({ msg: 'User deleted' })
+    } catch (err) {
+        console.error(err.message);
+        return res.status(500).send('Server error');
+    }
+});
+
+// @route PUT api/profile/experience
+// @desc Add profile experience
+// @access Private
+router.put('/experience', [ tokenAuth, [
+    check('title', 'Title is required').not().isEmpty(),
+    check('company', 'Company is required').not().isEmpty(),
+    check('from', 'From date is required').not().isEmpty()
+] ], async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ msg: errors.array() });
+    }
+
+    const {
+        title,
+        company,
+        location,
+        from,
+        to,
+        current,
+        description
+    } = req.body;
+
+    const newExperience = {
+        title,
+        company,
+        location,
+        from,
+        to,
+        current,
+        description
+    };
+
+    try {
+        const profile = await Profile.findOne({ user: req.user.id });
+        profile.experience.unshift(newExperience);
+        await profile.save();
+
+        res.json(profile);
+    } catch (err) {
+        console.error(err.message);
+        return res.status(500).send('Server error');
+    }
+})
+
 module.exports = router;
