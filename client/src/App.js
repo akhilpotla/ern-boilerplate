786cb52889e2c0942358baff66b29584f7001550
Akhil Potla <akhil4400@gmail.com>
2021-07-02 17:14:48 -0500

    Adding frontend with React and Redux

---
+++ client/src/App.js
@@ -0,0 +1,34 @@
+import React, { Fragment } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Alert from './components/layout/Alert';
+import Navbar from './components/layout/Navbar';
+import Landing from './components/layout/Landing';
+import Login from './components/auth/Login';
+import Register from './components/auth/Register';
+
+// Redux
+import { Provider } from 'react-redux';
+import store from './store';
+
+// Styles
+import './App.css';
+
+const App = () => (
+    <Provider store={store}>
+        <Router>
+            <Fragment>
+                <Navbar />
+                <Route exact path="/" component={Landing}/>
+                <section className="container">
+                    <Alert />
+                    <Switch>
+                        <Route exact path="/register" component={Register}/>
+                        <Route exact path="/login" component={Login}/>
+                    </Switch>
+                </section>
+            </Fragment>
+        </Router>
+    </Provider>
+);
+
+export default App;
---
+++ client/src/components/auth/Register.js
@@ -0,0 +1,91 @@
+import React, { Fragment, useState } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
+import { setAlert } from '../../actions/alert';
+
+const Register = ({ setAlert }) => {
+
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [password2, setPassword2] = useState('');
+
+    const onSubmit = async (e) => {
+        e.preventDefault();
+        if (password !== password2) {
+            setAlert('Passwords do not match', 'danger');
+        } else {
+            const formData = {name, email, password};
+
+            try {
+                const res = await axios.post('/api/users', formData);
+                console.log(res.data);
+            } catch (err) {
+                console.error(err.response.data);
+            }
+        }
+    }
+
+    return (
+        <Fragment>
+            <h1 className="large text-primary">Sign Up</h1>
+            <p className="lead"><i className="fas fa-user"></i> Create Your Account</p>
+            <form className="form" onSubmit={e => onSubmit(e)}>
+                <div className="form-group">
+                    <input
+                        type="text"
+                        placeholder="Name"
+                        name="name"
+                        value={name}
+                        onChange={(e) => setName(e.target.value)}
+                        required
+                    />
+                </div>
+                <div className="form-group">
+                    <input
+                        type="email"
+                        placeholder="Email Address"
+                        name="email"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                        required
+                    />
+                </div>
+                <div className="form-group">
+                    <input
+                        type="password"
+                        placeholder="Password"
+                        name="password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                        minLength="6"
+                        required
+                    />
+                </div>
+                <div className="form-group">
+                    <input
+                        type="password"
+                        placeholder="Confirm Password"
+                        name="password2"
+                        value={password2}
+                        onChange={(e) => setPassword2(e.target.value)}
+                        minLength="6"
+                        required
+                    />
+                </div>
+                <input type="submit" className="btn btn-primary" value="Register" />
+            </form>
+            <p className="my-1">
+                Already have an account? <Link to="/login">Sign In</Link>
+            </p>
+        </Fragment>
+    );
+}
+
+Register.propTypes = {
+    setAlert: PropTypes.func.isRequired
+};
+
+export default connect(null, { setAlert })(Register);
