8d573824d4fc2d23c8cd001dd4160d526def4ac2
Akhil Potla <akhil4400@gmail.com>
2021-09-08 16:14:18 -0500

    Using cors

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const connectDB = require('./config/db');
+const cors = require('cors');
 
 const app = express();
 
@@ -10,6 +11,7 @@ connectDB();
 app.use(express.json({
     extended: false
 }));
+app.use(cors());
 
 app.get('/', (req, res) => res.send('API Running'));
 
