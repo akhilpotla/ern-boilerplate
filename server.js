3612468c35fc8d21fe4fd2a18a04a7cbc90b9ead
Akhil Potla <akhil4400@gmail.com>
2021-09-13 22:04:51 -0500

    Removing references to posts and profile in server.js

--- server.js
+++ server.js
@@ -17,8 +17,6 @@ app.get('/', (req, res) => res.send('API Running'));
 
 // Define Routes
 app.use('/api/auth', require('./routes/api/auth'));
-app.use('/api/posts', require('./routes/api/posts'));
-app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/users', require('./routes/api/users'));
 
 
