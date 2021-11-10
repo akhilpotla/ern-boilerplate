7c49128a41c91071daad81a1a4398cb6cc7fe092
Akhil Potla <akhil4400@gmail.com>
2021-11-10 14:24:26 -0600

    Delete all reducer stuff

--- client/src/App.js
+++ client/src/App.js
@@ -2,16 +2,12 @@ import React, { Fragment } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import axios from 'axios';
 
-import Alert from './components/layout/Alert';
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
 import Login from './components/auth/Login';
 import Profile from './components/Profile';
 import Register from './components/auth/Register';
 
-// Redux
-import { Provider } from 'react-redux';
-import store from './store';
 
 // Styles
 import './App.css';
@@ -19,22 +15,19 @@ import './App.css';
 axios.defaults.baseURL = 'http://localhost:5000';
 
 const App = () => (
-    <Provider store={store}>
-        <Router>
-            <Fragment>
-                <Navbar />
-                <Route exact path="/" component={Landing}/>
-                <section className="container">
-                    <Alert />
-                    <Switch>
-                        <Route exact path="/register" component={Register}/>
-                        <Route exact path="/login" component={Login}/>
-                        <Route exact path="/profile" component={Profile}/>
-                    </Switch>
-                </section>
-            </Fragment>
-        </Router>
-    </Provider>
+    <Router>
+        <Fragment>
+            <Navbar />
+            <Route exact path="/" component={Landing}/>
+            <section className="container">
+                <Switch>
+                    <Route exact path="/register" component={Register}/>
+                    <Route exact path="/login" component={Login}/>
+                    <Route exact path="/profile" component={Profile}/>
+                </Switch>
+            </section>
+        </Fragment>
+    </Router>
 );
 
 export default App;
