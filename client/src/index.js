8f2cf7cbc7d0fa94a5f047ab69c870b195f85c84
Akhil Potla <akhil4400@gmail.com>
2022-10-01 12:48:00 -0600

    Using axios and setting baseURL

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import axios from 'axios';
+
+
+axios.defaults.baseURL = 'http://localhost:3000';
 
 ReactDOM.render(
   <React.StrictMode>
