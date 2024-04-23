255d60a383233a7bcd9f272ffa09eadb70231a74
Akhil Potla <akhil.potla@utah.edu>
2024-04-23 14:40:23 -0600

    Add table to the DB

--- config/db.mysql.js
+++ config/db.mysql.js
@@ -14,4 +14,23 @@ connection.connect(err => {
   console.log('Connected to the MySQL server.');
 });
 
+const sql = `
+CREATE TABLE IF NOT EXISTS users (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(255) NOT NULL,
+  password VARCHAR(255) NOT NULL,
+  email VARCHAR(255) UNIQUE,
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+`;
+
+connection.query(sql, function(err, results, fields) {
+  if (err) {
+    console.error('Failed to create table: ' + err);
+    return;
+  }
+  console.log('Table created or already exists.');
+});
+
+
 module.exports = connection;
