8c345fe2f3d324f341e7d7385980f35410fd65ea
Akhil Potla <akhil.potla@utah.edu>
2024-04-24 12:56:32 -0600

    Fixing redirect

--- client/src/components/Profile.js
+++ client/src/components/Profile.js
@@ -1,18 +1,15 @@
 import React from 'react';
-import { useNavigate } from 'react-router-dom';
+import { Navigate } from 'react-router-dom';
 
 import { isLoggedIn } from '../utils/setAuthToken';
 
 const Profile = () => {
 
-    let navigate = useNavigate();
-
     if (!isLoggedIn()) {
-        navigate('/login');
-        return null;
+        return <Navigate to="/login" replace />;
     } else {
         return (
-            <div style={{ marginTop: 200 }}>
+            <div>
                 <h3>This is the profile page. You are logged in.</h3>
             </div>
         );
