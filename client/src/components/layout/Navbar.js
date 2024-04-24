eccf3dd1b43691644fda1000ec060a2ca8ff4b04
Akhil Potla <akhil.potla@utah.edu>
2024-04-24 13:02:13 -0600

    Removing unused linked

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -8,7 +8,6 @@ const Navbar = () => {
                 <Link to="/"><i className="fas fa-code"></i> DevConnector</Link>
             </h1>
             <ul>
-                <li><Link to="/profiles">Developers</Link></li>
                 <li><Link to="/register">Register</Link></li>
                 <li><Link to="/login">Login</Link></li>
             </ul>
