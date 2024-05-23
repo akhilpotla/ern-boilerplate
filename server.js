86911724ee982fb28a0160277c5499137887214e
Akhil Potla <akhil.potla@utah.edu>
2024-05-23 13:20:03 -0600

    Testing setup

--- server.js
+++ server.js
@@ -81,3 +81,5 @@ app.use('/api/users', require('./routes/api/users'));
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+
+module.exports = app;
---
+++ tests/users.test.js
@@ -0,0 +1,25 @@
+const request = require('supertest');
+
+const app = require('../server');
+const { sequelize } = require('../config/db');
+
+beforeAll(async () => {   
+    await sequelize.authenticate();
+    await sequelize.sync({ alter: true });
+});
+
+afterAll(async () => {
+    await sequelize.drop();
+    await sequelize.close();
+});
+
+describe('POST /api/users', () => {
+    it('Should register a user', async () => {
+        const res = await request(app).post('/api/users').send({
+            name: 'john',
+            email: 'john@example.com',
+            password: 'password'
+        });
+        expect(res.statusCode).toBe(200);
+    });
+});
