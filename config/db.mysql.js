12150a6218f348acf035b3ccd52bea4b30250eff
Akhil Potla <akhil.potla@utah.edu>
2024-05-23 13:19:53 -0600

    Breaking database tables into multiple files

--- config/db.mysql.js
+++ config/db.mysql.js
@@ -15,26 +15,12 @@ sequelize.authenticate().then(() => {
   console.error('Unable to connect to the database: ', error);
 });
 
-const User = sequelize.define('users', {
-  name: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  password: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  email: {
-    type: DataTypes.STRING,
-    allowNull: false,
-    unique: true
-  }
-});
+const User = require('../models/User')(sequelize, DataTypes);
 
 sequelize.sync({ alter: true }).then(() => {
   console.log('User table created successfully!');
 }).catch((error) => {
   console.error('Unable to create table: ', error);
 });
- 
+
 module.exports = { sequelize, User };
